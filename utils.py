9a881f01a5c94a352d739cbfca4414e817d21142
Chiyoung Won <wcyoung98@gmail.com>
2020-09-27 14:59:17 +0900

    Response 파일정보에 파일타입 추가

--- utils.py
+++ utils.py
@@ -7,8 +7,18 @@ def to_datetime(timestamp):
 
 
 def get_file_info(file_path):
+    file_type = 'Unknown'
+
+    if os.path.isdir(file_path):
+        file_type = 'directory'
+    elif os.path.islink(file_path):
+        file_type = 'link'
+    elif os.path.isfile(file_path):
+        file_type = 'file'
+
     return {
         "name": os.path.basename(file_path),
+        "type": file_type,
         "size": os.path.getsize(file_path),
         "ctime": to_datetime(os.path.getctime(file_path)),
         "mtime": to_datetime(os.path.getmtime(file_path))
